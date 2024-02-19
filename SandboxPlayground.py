2e77327645f92ea4a499ab98032690ba291954c6
Ethan Mingerink <etmingerink@yahoo.com>
2024-02-19 07:48:51 -0500

    complete tringale

---
+++ SandboxPlayground.py
@@ -0,0 +1,91 @@
+# Started from Tello Template
+# This Python app is in the Public domain
+# Some parts from Tello3.py
+
+import threading, socket, sys, time, subprocess
+
+
+# GLOBAL VARIABLES DECLARED HERE....
+host = ''
+port = 9000
+locaddr = (host,port)
+tello_address = ('192.168.10.1', 8889) # Get the Tello drone's address
+
+
+
+# Creates a UDP socketd
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(locaddr)
+
+
+def recv():
+    count = 0
+    while True:
+        try:
+            data, server = sock.recvfrom(1518)
+            print(data.decode(encoding="utf-8"))
+        except Exception:
+            print ('\n****Keep Eye on Drone****\n')
+            break
+
+
+def sendmsg(msg, sleep = 6):
+    print("Sending: " + msg)
+    msg = msg.encode(encoding="utf-8")
+    sock.sendto(msg, tello_address)
+    time.sleep(sleep)
+
+# recvThread create
+recvThread = threading.Thread(target=recv)
+recvThread.start()
+
+
+# CREATE FUNCTIONS HERE....
+
+
+print("\nEthan Mingerink")
+print("Program Name: Square")
+print("Date: 2.6.24 ")
+print("Drone Wifi: E681")
+print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
+print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
+ready = input('\nAre you ready to take flight: ')
+
+
+try:
+    if ready.lower() == 'yes':
+        print("\nStarting Drone!\n")
+
+        sendmsg('command', 0)
+        sendmsg('battery?', 6)
+        sendmsg('takeoff')
+
+
+        sendmsg('up 65', 10)
+        sendmsg('forward 500', 10)
+
+        #Square - Pilot = Logan Talor - CoPilot = Ethan Mingerink
+        # for i in range(4):
+        #   sendmsg('forward 50', 8)
+        #   sendmsg('cw 90', 8)
+
+        #Triangle - Pilot = Ethan Mingerink - CoPilot = Logan Talor
+
+        # for i in range(3):
+        #   sendmsg('forward 50',6)
+        #   sendmsg('cw 135', 6)
+
+        # Circle - pilot = Logan Talor - CoPilot = Ethan Mingerink
+
+        sendmsg('land')
+
+        print('\nGreat Flight!!!')
+
+    else:
+        print('\nMake sure you check WIFI, surroundings, co-pilot is ready, re-run program\n')
+except KeyboardInterrupt:
+    sendmsg('emergency')
+
+breakr = True
+sock.close()
